febb1421cb00342a4e4b1287e40f6a50cb466328
serhatduman <serhat.dum@outlook.com>
2023-05-13 23:37:03 -0500

    acsafsfwsdv

---
+++ deneme.java
@@ -0,0 +1,11 @@
+public class deneme {
+
+    public static void main(String[] args) {
+
+        System.out.println("hayat sana guzel");
+
+        System.out.println("ilk oyu ben kullandim " + 18);
+
+
+    }
+}
